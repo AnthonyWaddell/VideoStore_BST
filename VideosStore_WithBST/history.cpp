8ad78fc78e0b24f255afdf5691fa9aa6ea19f3f4
salazc2 <34729472+salazc2@users.noreply.github.com>
2018-03-09 16:51:56 -0800

    Update history.cpp

--- VideosStore_WithBST/history.cpp
+++ VideosStore_WithBST/history.cpp
@@ -1,8 +1,48 @@
 #include "history.h"
+#include <iostream>
+
+using namespace std;
+
+
 
 // --------------------------------------------------------------------------
 //
 // --------------------------------------------------------------------------
 History::~History()
 {
+
+
+
+}
+
+bool History::processAction(std::ifstream& inputFile, IStore* store)
+{
+	char action = ' '; // not sure if needed 
+	string badData;
+	int id;
+	bool returnValue;
+
+	inputFile >> action;
+	inputFile >> id;
+
+	if (store->getCustomerHashTablePtr()->retrieve(id) == NULL)
+	{
+		cout << "Incorrect Customer ID." << endl;
+		getline(inputFile, badData); //get the rest of the data of the wrong ID
+		returnValue = false;
+		
+		
+	}
+	else if (store->getCustomerHashTablePtr()->retrieve(id) != NULL)
+	{
+		IPerson* historyPerson;
+		static_cast<Customer*>(historyPerson) = store->getCustomerHashTablePtr()->retrieve(id);
+		//historyPerson check its history
+	}
+
+
+
+	return returnValue;
+
+
 }
