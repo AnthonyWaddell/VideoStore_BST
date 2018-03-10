8f8323f50d7f901be3a5ce0b40b4cfe364f30d8f
salazc2 <34729472+salazc2@users.noreply.github.com>
2018-03-09 18:04:25 -0800

    Update history.cpp

--- VideosStore_WithBST/history.cpp
+++ VideosStore_WithBST/history.cpp
@@ -4,20 +4,28 @@
 using namespace std;
 
 
-
 // --------------------------------------------------------------------------
 //
 // --------------------------------------------------------------------------
 History::~History()
 {
-
-
-
 }
 
+
+// ---------------------------- processAction -------------------------------
+// Processes the history action reading what to do from a input file.
+// Preconditions: A valid IStore object. Correctly formatted input file line.
+// Postconditions: Input file ifstream will be at the end of the line.
+// Prints to the screen the customers history, if valid customer.
+// Parameter: inputFile - A input file with lines of properly formatted
+// commands.
+// store - An IStore object.
+// Return: Returns true if history displaying was successful, and false 
+// otherwise (invalid customer).
+// --------------------------------------------------------------------------
 bool History::processAction(std::ifstream& inputFile, IStore* store)
 {
-	char action = ' '; // not sure if needed 
+	char action = ' '; 
 	string badData;
 	int id;
 	bool returnValue;
@@ -30,19 +38,15 @@ bool History::processAction(std::ifstream& inputFile, IStore* store)
 		cout << "Incorrect Customer ID." << endl;
 		getline(inputFile, badData); //get the rest of the data of the wrong ID
 		returnValue = false;
-		
-		
+				
 	}
 	else if (store->getCustomerHashTablePtr()->retrieve(id) != NULL)
 	{
 		IPerson* historyPerson;
 		static_cast<Customer*>(historyPerson) = store->getCustomerHashTablePtr()->retrieve(id);
-		//historyPerson check its history
+		returnValue = true;
+		//historyPerson->getDVDHistory;
 	}
 
-
-
 	return returnValue;
-
-
 }
