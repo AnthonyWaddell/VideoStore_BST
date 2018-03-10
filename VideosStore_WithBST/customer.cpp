0293d5e23db15cc146de2223f7b333cb51d122f1
salazc2 <34729472+salazc2@users.noreply.github.com>
2018-03-09 18:02:05 -0800

    Update customer.cpp

--- VideosStore_WithBST/customer.cpp
+++ VideosStore_WithBST/customer.cpp
@@ -1,4 +1,8 @@
 #include "customer.h"
+#include <iostream>
+
+using namespace std;
+
 
 // --------------------------------------------------------------------------
 // constructor with parameters
@@ -25,6 +29,11 @@ Customer::~Customer()
 // --------------------------------------------------------------------------
 stack<string> Customer::getDVDHistory()
 {
+	while (!m_dvdHistory.empty())
+	{
+	    m_dvdHistory.pop();
+	    cout << "\n"; // go to next line
+	}
 	return m_dvdHistory;
 }
 //
@@ -35,3 +44,15 @@ stack<string> Customer::getDVDHistory()
 //{
 //	return m_inventoryLLPtr;
 //}
+
+// ---------------------------- setHistory ----------------------------------
+// Pushes a borrow or return information into m_dvdHistory which is the stack
+// of strings that contains customers history.
+// Preconditions: None.
+// Postconditions: m_dvdHistory contains newly added history line that is
+// a borrow or a return with the information.
+// --------------------------------------------------------------------------
+void Customer::setHistory(string historyLine)
+{
+	m_dvdHistory.push(historyLine);
+}
