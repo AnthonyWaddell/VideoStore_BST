6ab7a8e5a956dd06eeb05bdb52b51f4cdac4438d
salazc2 <34729472+salazc2@users.noreply.github.com>
2018-03-12 12:57:14 -0700

    Update iperson.cpp

--- VideosStore_WithBST/iperson.cpp
+++ VideosStore_WithBST/iperson.cpp
@@ -1,14 +1,32 @@
+//-----------------------------------------------------------------------------
+// File:	iperson.cpp
+// Programmer:	Anthony Waddell, Christopher Salazar
+// Description: The iperson class is a base class that can be extended to be any 
+// sort of person. The iperson is the base class for our customer class that is 
+// a person. Iperson can hold a name and a hashkey to be ineserted in a hashtable.
+//-----------------------------------------------------------------------------
+
 #include "iperson.h"
 
 //--------------------------------------------------------------------------
-// constructor 
+// Function:	IPerson::IPerson()
+// Title:	Contructor
+// Description: Contructor
+// Parameters:	N/A
+// Returns:	N/A
+// History Log: 03/14/18 AW Completed Function
 // --------------------------------------------------------------------------
 IPerson::IPerson()
 {
 }
 
 // --------------------------------------------------------------------------
-// constructor with parameters
+// Function:	IPerson::IPerson()
+// Title:	constructor with parameters
+// Description: Contructor that takes in a first and last name along with a hashkey
+// Parameters:	string firstName, string lastName, int hashKey
+// Returns:	N/A
+// History Log: 03/14/18 AW Completed Function
 // --------------------------------------------------------------------------
 IPerson::IPerson(string firstName, string lastName, int hashKey)
 {
@@ -18,14 +36,24 @@ IPerson::IPerson(string firstName, string lastName, int hashKey)
 }
 
 // --------------------------------------------------------------------------
-// destructor
+// Function:	IPerson::~Person()
+// Title:	Destructor
+// Description: Destructor
+// Parameters:	N/A
+// Returns:	N/A
+// History Log: 03/14/18 AW Completed Function
 // --------------------------------------------------------------------------
 IPerson::~IPerson()
 {
 }
 
 // --------------------------------------------------------------------------
-// accessor for m_hashKey
+// Function:	int IPerson::getHashKey()
+// Title:	accessor for m_hashKey
+// Description: get the hashkey
+// Parameters:	N/A
+// Returns:	int
+// History Log: 03/14/18 AW Completed Function
 // --------------------------------------------------------------------------
 int IPerson::getHashKey()
 {
@@ -33,7 +61,12 @@ int IPerson::getHashKey()
 }
 
 // --------------------------------------------------------------------------
-// mutator for m_hashKey
+// Function:	void IPerson::setHashKey(int key)
+// Title:	mutator for m_hashKey
+// Description: set the hashkey of a person
+// Parameters:	int key
+// Returns:	void
+// History Log: 03/14/18 AW Completed Function
 // --------------------------------------------------------------------------
 void IPerson::setHashKey(int key)
 {
@@ -44,7 +77,12 @@ void IPerson::setHashKey(int key)
 }
 
 // --------------------------------------------------------------------------
-// accessor for m_firstName
+// Function:	string IPerson::getFirstName()
+// Title:	accessor for m_firstName
+// Description: get the first name of an iperson
+// Parameters:	N/A
+// Returns:	string
+// History Log: 03/14/18 AW Completed Function
 // --------------------------------------------------------------------------
 string IPerson::getFirstName()
 {
@@ -52,7 +90,13 @@ string IPerson::getFirstName()
 }
 
 // --------------------------------------------------------------------------
-// accessor for m_lastName
+// 
+// Function:	string IPerson::getLastName()
+// Title:	accessor for m_lastName
+// Description: get the last name of an iperson
+// Parameters:	N/A
+// Returns:	string
+// History Log: 03/14/18 AW Completed Function
 // --------------------------------------------------------------------------
 string IPerson::getLastName()
 {
